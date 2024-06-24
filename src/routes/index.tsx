8b2636925a99fafa8f3822068e3d52b1a7fe3c44
taichi221228 <taichi221228@icloud.com>
2024-06-24 13:47:39 +0900

    feat: Add VelvetBanana component
    This commit introduces a new VelvetBanana component, which displays a SVG image. Along with the SVG path data, the component also includes necessary accessibility attributes such as 'role' and 'aria-label' for making the component accessible. CSS for positioning and hover effects has also been added.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,42 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import { VelvetBanana } from "~/components/velvet-banana/velvet-banana";
 import { css } from "~/styled-system/css";
 
 /** @private */
 export default component$(() => {
 	return (
-		<div
-			class={css({
-				position: "absolute",
-				insetBlockEnd: "50%",
-				insetInlineEnd: "0",
-				transform: "rotate(90deg) translate3d(50%, 0, 0)",
-				transformOrigin: "100%",
-			})}
-		>
-			<input
-				type="range"
-				min="0"
-				max="10"
-				step="1"
-				value={1}
-				onChange$={(e) => {
-					// const element = e.target as HTMLInputElement | null;
-					// if (element) speed.value = Number(element.value);
-				}}
+		<>
+			<VelvetBanana
+				class={css({
+					position: "absolute",
+					inset: 0,
+					margin: "auto",
+				})}
 			/>
-		</div>
+			<div
+				class={css({
+					position: "absolute",
+					insetBlockEnd: "50%",
+					insetInlineEnd: "0",
+					transform: "rotate(90deg) translate3d(50%, 0, 0)",
+					transformOrigin: "100%",
+				})}
+			>
+				<input
+					type="range"
+					min="0"
+					max="10"
+					step="1"
+					value={1}
+					onChange$={(e) => {
+						// const element = e.target as HTMLInputElement | null;
+						// if (element) speed.value = Number(element.value);
+					}}
+				/>
+			</div>
+		</>
 	);
 });
 
