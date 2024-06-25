126db99bc8664ca5e196861dd9b316fa5e2d230d
taichi221228 <taichi221228@icloud.com>
2024-06-25 12:07:37 +0900

    refactor: Refactor 'Raw' component and update Prop types in Bananas module
    Moved the 'easing' function in 'Raw' component to a constant outside 'Prop' type definition. Also, the 'Props' from 'Raw' is now exported for external use, and is being used as a type for 'BananasProps' in 'Bananas' module.

--- src/integrations/react/bananas/bananas.tsx
+++ src/integrations/react/bananas/bananas.tsx
@@ -1,5 +1,7 @@
 import { qwikify$ } from "@builder.io/qwik-react";
 
-import { Raw } from "./raw";
+import { Raw, type Props } from "./raw";
+
+export type BananasProps = Props;
 
 export const Bananas = qwikify$(Raw, { eagerness: "load" });
--- src/integrations/react/bananas/raw.tsx
+++ src/integrations/react/bananas/raw.tsx
@@ -8,15 +8,17 @@ import { css } from "~/styled-system/css";
 
 import { Banana } from "./raw/banana";
 
-type Props = {
+const easing = (x: number) => Math.sqrt(1 - (x - 1) ** 2);
+
+/** @package */
+export type Props = {
 	speed?: number;
 	count?: number;
 	depth?: number;
-	easing?: (x: number) => number;
 };
 
 /** @package */
-export const Raw = ({ speed = 1, count = 80, depth = 80, easing = (x: number) => Math.sqrt(1 - (x - 1) ** 2) }: Props) => (
+export const Raw = ({ speed = 1, count = 80, depth = 80 }: Props) => (
 	<div className={css({ position: "fixed", zIndex: -1, inset: 0, animation: "fade 6s normal forwards ease-in-out" })}>
 		<Canvas flat gl={{ antialias: false }} dpr={[1, 1.5]} camera={{ position: [0, 0, 10], fov: 20, near: 0.01, far: depth + 15 }}>
 			<color attach="background" args={["#ffd863"]} />
